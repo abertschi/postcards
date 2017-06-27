f1865be284cce96565e428384bd2a2ebcab1b763
Andrin <andrin.bertschi@gmail.com>
2017-06-27 21:17:49 +0200

    clean up

--- postcards.py
+++ postcards.py
@@ -120,7 +120,6 @@ class Postcards:
             return json.load(f)
 
     def _read_picture(self, location):
-        print(location)
         if location.startswith('http'):
             headers = {
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
