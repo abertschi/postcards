baa8dd63b83b43489e8735f5277b417fcad58985
Andrin <andrin.bertschi@gmail.com>
2017-06-27 18:08:35 +0200

    add help

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # Postcards
 > A CLI for the Swiss Postcard Creator
+
+## Install
+- Not yet available on pip
+
+## Usage
+```sh
+python postcards.py --help 
+usage: postcards.py [-h] [--config-file CONFIG_FILE]
+                    [--recipient-file RECIPIENT_FILE]
+                    [--sender-file SENDER_FILE]
+                    [--accounts-file ACCOUNTS_FILE] [--image IMAGE]
+                    [--message MESSAGE] [--key KEY] [--username USERNAME]
+                    [--password PASSWORD] [--encrypt ENCRYPT ENCRYPT]
+                    [--decrypt DECRYPT DECRYPT] [--mock] [--trace]
+
+Postcards is a CLI for the Swiss Postcard Creator
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --config-file CONFIG_FILE
+                        location to the json config file
+  --recipient-file RECIPIENT_FILE
+                        location to a dedicated json file containing the recipient
+  --sender-file SENDER_FILE
+                        location to a dedicated json file containing the sender
+  --accounts-file ACCOUNTS_FILE
+                        location to a dedicated json file containing postcard creator accounts
+  --image IMAGE         postcard picture. path to an URL or image on disk
+  --message MESSAGE     postcard message
+  --key KEY             a key to decrypt credentials stored in config files: --key <password>
+  --username USERNAME   username credential. otherwise set in config or accounts file
+  --password PASSWORD   password credential. otherwise set in config or accounts file
+  --encrypt ENCRYPT ENCRYPT
+                        encrypt credentials to store in config files: --encrypt <key> <credential>
+  --decrypt DECRYPT DECRYPT
+                        decrypt credentials: --decrypt <key> <credential>
+  --mock                do not submit postcard. useful for testing
+  --trace               enable tracing. useful for testing
+
+example use:
+
+use a config file:
+        postcards.py --config-file config.json 
+                     --key <key_to_decrypt_credentials> \
+                     --message 'My postcard' --picture ./picture.jpg
+
+pass credentials as arguments and use dedicated config files:
+        postcards.py --sender-file sender.json \
+          --recipient-file recipient.json \
+          --username <username> \
+          --password <password> \
+          --message message \
+          --picture ./picture.jpg
+
+sample config file:
+
+{
+  "recipient": {
+    "firstname": "",
+    "lastname": "",
+    "street": "",
+    "zipcode": "",
+    "city": ""
+  },
+  "sender": {
+    "firstname": "",
+    "lastname": "",
+    "street": "",
+    "zipcode": "",
+    "city": ""
+  },
+  "accounts": [
+    {
+      "username": "",
+      "password": ""
+    }
+  ]
+}
+
+github: https://github.com/abertschi/postcards
+Andrin Bertschi 2017, MIT
+
+```
+## Plugins
+
+### Send pictures from a folder
+
+### Send pictures from http://pexels.com
+
+## Related
+- [postcard_creator_wrapper](https://github.com/abertschi/postcard_creator_wrapper) - Python API wrapper around the Swiss Postcard Creator
+
+## Author
+Andrin Bertschi
+
+## License
+
+MIT
--- postcards.py
+++ postcards.py
@@ -3,58 +3,47 @@ import sys
 import base64
 import json
 import os
-import argparse
+import argparse, textwrap
+from argparse import RawTextHelpFormatter
 
 postcard_creator.Debug.debug = True
 
 
 class Postcards:
-
     def main(self, argv):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--key', nargs=1, default=False)
-        parser.add_argument('--encrypt', action="store", nargs=2, default=False)
-        parser.add_argument('--decrypt', action="store", nargs=2, default=False)
-        parser.add_argument('--config-file', default=os.path.dirname(os.path.realpath(__file__)) + '/config.json')
-        parser.add_argument('--recipient-file', default=False)
-        parser.add_argument('--sender-file', default=False)
-        parser.add_argument('--accounts-file', default=False)
-        parser.add_argument('--mock', action='store_true')
-        parser.add_argument('--username', default=False)
-        parser.add_argument('--password', default=False)
-        parser.add_argument('--image', default=False)
-        parser.add_argument('--message', default=False)
-        parser.add_argument('--config-json', default=False)
-        parser.add_argument('--payload-json', default=False)
-        args = parser.parse_args()
-
+        args = self.get_argparser(argv)
         if args.encrypt:
             print(self._encrypt(args.encrypt[0], args.encrypt[1]))
             exit(0)
 
         config = self._read_config(args.config)
-        accounts = []
-        for account in config.get('accounts'):
-            accounts.append({
-                'username': account.get('username') if not args.key else self._decrypt(args.key,
-                                                                                       account.get('username')),
-                'password': account.get('password') if not args.key else self._decrypt(args.key,
-                                                                                       account.get('password'))
-            })
+        accounts = self._get_accounts_from_config(config)
 
         if not accounts:
             print('No account given')
             exit(1)
+
         if not config.get('recipient'):
             print('No recipient sent in config file')
             exit(1)
 
+        if not self._is_plugin():
+            message = args.message
+            picture = args.picture
+            if not picture:
+                print('No --picture set. Run a plugin or set --picture')
+                exit(1)
+            picture = self._read_picture(picture)
+
         self.send(accounts=accounts,
                   recipient=config.get('recipient'),
                   sender=config.get('sender') if config.get('sender') else config.get('recipient'),
-                  mock=str(args.mock), plugin_payload=config.get('payload'))
+                  mock=bool(args.mock),
+                  plugin_payload=config.get('payload'),
+                  picture_stream=picture,
+                  message=str(message))
 
-    def send(self, accounts, recipient, sender, mock=False, plugin_payload={}):
+    def send(self, accounts, recipient, sender, mock=False, plugin_payload={}, message=None, picture_stream=None):
         pcc_wrapper = None
         for account in accounts:
             token = postcard_creator.Token()
@@ -68,11 +57,14 @@ class Postcards:
             print('No valid account given. Run later again or check accounts')
             exit(1)
 
-        img_and_text = self.get_img_and_text(plugin_payload)
-        card = postcard_creator.Postcard(message=img_and_text['text'], recipient=self.create_recipient(recipient),
-                                         sender=self.create_sender(sender),
-                                         picture_stream=img_and_text['img'])
+        if self._is_plugin():
+            img_and_text = self.get_img_and_text(plugin_payload)
+            message = img_and_text['text']
+            picture_stream = img_and_text['img']
 
+        card = postcard_creator.Postcard(message=message, recipient=self.create_recipient(recipient),
+                                         sender=self.create_sender(sender),
+                                         picture_stream=picture_stream)
         pcc_wrapper.send_free_card(card, mock_send=mock)
 
     def create_recipient(self, recipient):
@@ -89,9 +81,23 @@ class Postcards:
                                        zip_code=sender.get('zipcode'),
                                        place=sender.get('city'))
 
+    def _get_accounts_from_config(self, config, key=None):
+        accounts = []
+        for account in config.get('accounts'):
+            accounts.append({
+                'username': account.get('username') if not key
+                else self._decrypt(key, account.get('username')),
+                'password': account.get('password') if not key
+                else self._decrypt(key, account.get('password'))
+            })
+        return accounts
+
     def _read_config(self, location):
         with open(location) as f:
             return json.load(f)
+
+    def _read_picture(self, location):
+        return ''
 
     def _encrypt(self, key, msg):
         return self._encode(key, msg.encode('utf-8')).decode('utf-8')
@@ -116,7 +122,7 @@ class Postcards:
             dec_c = chr((256 + enc[i] - ord(key_c)) % 256)
             dec.append(dec_c)
 
-    def _has_plugin(self):
+    def _is_plugin(self):
         try:
             self.get_img_and_text(None)
             return True
@@ -131,6 +137,90 @@ class Postcards:
         """
         raise Exception('Dont run this class directly. Use a plugin instead')
         return {'img': None, 'text': None}
+
+    def get_argparser(self, argv):
+        parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
+                                         description='Postcards is a CLI for the Swiss Postcard Creator')
+        parser.add_argument('--config-file', default=os.path.dirname(os.path.realpath(__file__)) + '/config.json',
+                            help='location to the json config file')
+        parser.add_argument('--recipient-file', default=False,
+                            help='location to a dedicated json file containing the recipient')
+        parser.add_argument('--sender-file', default=False,
+                            help='location to a dedicated json file containing the sender')
+        parser.add_argument('--accounts-file', default=False,
+                            help='location to a dedicated json file containing postcard creator accounts')
+
+        parser.add_argument('--image', default=False,
+                            help='postcard picture. path to an URL or image on disk')
+        parser.add_argument('--message', default='',
+                            help='postcard message')
+        parser.add_argument('--key', nargs=1, default=False,
+                            help='a key to decrypt credentials stored in config files: --key <password>')
+
+        parser.add_argument('--username', default=False,
+                            help='username credential. otherwise set in config or accounts file')
+        parser.add_argument('--password', default=False,
+                            help='password credential. otherwise set in config or accounts file')
+
+        parser.add_argument('--encrypt', action="store", nargs=2, default=False,
+                            help='encrypt credentials to store in config files: --encrypt <key> <credential>')
+        parser.add_argument('--decrypt', action="store", nargs=2, default=False,
+                            help='decrypt credentials: --decrypt <key> <credential>')
+
+        parser.add_argument('--mock', action='store_true',
+                            help='do not submit postcard. useful for testing')
+        parser.add_argument('--trace', action='store_true',
+                            help='enable tracing. useful for testing')
+
+        parser.epilog = textwrap.dedent('''\
+
+                example use:
+
+                use a config file:
+                \tpostcards.py --config-file config.json 
+                \t             --key <key_to_decrypt_credentials> \\
+                \t             --message 'My postcard' --picture ./picture.jpg
+
+                pass credentials as arguments and use dedicated config files:
+                \tpostcards.py --sender-file sender.json \\
+                \t  --recipient-file recipient.json \\
+                \t  --username <username> \\
+                \t  --password <password> \\
+                \t  --message message \\
+                \t  --picture ./picture.jpg
+
+
+                sample config file:
+
+                {
+                  "recipient": {
+                    "firstname": "",
+                    "lastname": "",
+                    "street": "",
+                    "zipcode": "",
+                    "city": ""
+                  },
+                  "sender": {
+                    "firstname": "",
+                    "lastname": "",
+                    "street": "",
+                    "zipcode": "",
+                    "city": ""
+                  },
+                  "accounts": [
+                    {
+                      "username": "",
+                      "password": ""
+                    }
+                  ]
+                }
+
+
+                github: https://github.com/abertschi/postcards
+                Andrin Bertschi 2017, MIT
+
+                ''')
+        return parser.parse_args()
 
 
 if __name__ == '__main__':
